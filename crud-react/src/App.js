f72c719540c21569f06de1b04aa8b5634d8a70c9
oguzhanascier <oguzhanascier20@gmail.com>
2023-03-17 17:32:23 +0300

    reload button

--- crud-react/src/App.js
+++ crud-react/src/App.js
@@ -1,4 +1,4 @@
-import { useEffect,useState } from 'react';
+import { useEffect, useState } from 'react';
 import axios from 'axios';
 import './App.css';
 import Courses from './Courses'
@@ -9,18 +9,18 @@ function App() {
   const [courses, setCourses] = useState([])
   const [loading, setLoading] = useState(true)
 
-const deleteCourse=(id)=>{
-  const newCoursesList= courses.filter((course)=> course.id !== id)
-  setCourses(newCoursesList)
-}
+  const deleteCourse = (id) => {
+    const newCoursesList = courses.filter((course) => course.id !== id)
+    setCourses(newCoursesList)
+  }
 
   const fetchCourses = async () => {
     try {
-    const res = await axios.get('http://localhost:3000/courses')
-    setCourses(res.data)
-    setLoading(false)
+      const res = await axios.get('http://localhost:3000/courses')
+      setCourses(res.data)
+      setLoading(false)
     } catch (error) {
-    setLoading(false)
+      setLoading(false)
       console.log(error)
     }
   }
@@ -30,8 +30,13 @@ const deleteCourse=(id)=>{
 
   return (
     <div className="App">
-     { loading ? (<Loading/>) : (  <Courses courses={courses}  removeCourse={deleteCourse}/>)}
-    
+      {loading ? (<Loading />) :
+        <>
+          {courses.length === 0 ? (<div className='re'><button className='reload'>Kurs Yükle</button> </div>) : (<Courses courses={courses} removeCourse={deleteCourse} />)}
+        </>
+      }
+
+
     </div>
   );
 }
